d84458bcacfc649b2312bda020da5864b7e98eff
ergefakhreza <ergefakhreza@gmail.com>
2024-09-21 13:43:14 +0800

    Add files via upload

---
+++ Salam.java
@@ -0,0 +1,12 @@
+package halo;
+
+public class Salam {
+
+    public static void main(String[] args) {
+        String negara = "Indonesia";
+        short jmlProv = 38;
+
+        System.out.println("Halo " + negara);
+        System.out.println("Jumlah provinsi anda adalah " + jmlProv);
+    }
+}
